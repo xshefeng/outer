216b3148e74a6adc4b6b2f0be6cbe80a7716fd23
hefeng <503614875@qq.com>
2023-07-23 07:04:25 +0800

    create project

---
+++ src/main/java/com/francis/component/outer/OuterApplication.java
@@ -0,0 +1,13 @@
+package com.francis.component.outer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OuterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/francis/component/outer/OuterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francis.component.outer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
